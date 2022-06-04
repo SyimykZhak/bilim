4ee9ff3fb185a6a435dd62f03dda29ea12ea4085
Syimyk_Jakshybaev <74294748+SyimykZhak@users.noreply.github.com>
2022-06-04 10:53:38 +0300

    Update settings.py

--- bilim/settings.py
+++ bilim/settings.py
@@ -20,13 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5ox*0u(7le5obqzc71%m*d_9(%h@!9!wvkj#v!i$wenygrgroj'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
